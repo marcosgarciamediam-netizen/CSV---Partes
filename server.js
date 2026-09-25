c3b11fa24e3523a9baf776719771ed54840d2323
marcosgarciamediam-netizen <marcosgarciamediam@gmail.com>
2026-09-25 17:51:21 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 
 const app = express();
 app.use(cors());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'layoutcsv')));
 app.use(express.json());
 
 // Configuración unificada de PostgreSQL
